bc10e4ff8529217d9600d55d00b577f0d921fff5
AndyScorp <scorpionand28@gmail.com>
2018-01-01 14:21:21 +0200

    part 8

--- part_1/main.js
+++ part_1/main.js
@@ -12,7 +12,14 @@ if (canvas.getContext) {
     ctx.moveTo(100, 300);
     ctx.lineTo(200, 100);
     ctx.lineTo(250, 210);
-//    ctx.stroke();
+    ctx.lineTo(100, 300);
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.fillStyle = "blue"
+    ctx.arc(220, 130, 80, 0, 2*Math.PI);
+    ctx.stroke();
     ctx.fill();
 
 }
