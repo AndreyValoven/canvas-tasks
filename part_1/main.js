da2b22fadf082cc1d11e55439d94d8b837efeb4e
AndyScorp <scorpionand28@gmail.com>
2018-01-04 20:55:47 +0200

    part 5

--- part_1/main.js
+++ part_1/main.js
@@ -18,10 +18,24 @@ if (canvas.getContext) {
 
     for(let x = 0; x < 10; x++) {
         ctx.lineWidth = 1 + x;
-        ctx.beginPath();
         ctx.strokeStyle = `rgb(${Math.ceil(x * 20)},  0, 0)`;
+        ctx.lineCap = 'squere';
+
+        ctx.beginPath();
         ctx.moveTo(50 + (x * 20), 150);
         ctx.lineTo(50 + (x * 20), 270);
         ctx.stroke();
     }
+
+    ctx.beginPath();
+    ctx.setLineDash([5]);
+    ctx.rect(50, 300, 200, 100);
+    ctx.stroke();
+
+    var lngrade = ctx.createLinearGradient(60, 0, 450, 500);
+    lngrade.addColorStop(0, 'blue');
+    lngrade.addColorStop(1, 'red');
+
+    ctx.fillStyle = lngrade;
+    ctx.fillRect(300, 200, 300, 200);
 }
